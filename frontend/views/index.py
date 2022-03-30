21be0dee0891f0c26d4f81183ec2c63b4741cfaf
astra <zorkin.roman@gmail.com>
2022-03-30 17:22:13 +0300

    Review correct

--- frontend/views/index.py
+++ frontend/views/index.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template
 
-from frontend import config, measurements_client
+from frontend import measurements_client
 
 view = Blueprint('index', __name__)
-app_config = config.load_from_env()
 
 
 @view.route('/')
