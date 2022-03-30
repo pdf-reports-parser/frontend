38b8be171e6d158425b64b086cf0add72ff1a297
astra <zorkin.roman@gmail.com>
2022-03-30 15:17:26 +0300

    Index page up

--- frontend/__main__.py
+++ frontend/__main__.py
@@ -1,21 +1,22 @@
 import logging
 
-from typer import Typer
+#from typer import Typer
 
 from frontend import config
 from frontend.app import create_app
 
 logging.basicConfig(level=logging.DEBUG)
 
-typer_app = Typer()
+#typer_app = Typer()
 app_config = config.load_from_env()
 
 
-@typer_app.command(help='Start base service')
+#@typer_app.command(help='Start base service')
 def run():
     app = create_app()
     app.run(host=app_config.host, port=app_config.port, debug=False)
 
 
 if __name__ == '__main__':
-    typer_app()
+    #typer_app()
+    run()
--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
+from frontend.view import index
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(index.view)
 
     return app
 
