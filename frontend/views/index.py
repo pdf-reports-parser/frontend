9f6c70e54b045b3f91a4247f46010d4d5eb27a5b
astra <zorkin.roman@gmail.com>
2022-04-04 19:24:56 +0300

    implement flask wtf form

--- frontend/views/index.py
+++ frontend/views/index.py
@@ -1,26 +1,27 @@
 import httpx
 from flask import Blueprint, flash, render_template, redirect, request, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
+
 
 from frontend.client.measurements import client
 
 view = Blueprint('index', __name__)
 
 
-@view.route('/')
+class FileForm(FlaskForm):
+    pdf_report = FileField()
+
+
+@view.route('/', methods=['GET', 'POST'])
 def index():
     measurements = client.get_all()
-
-    return render_template(
-        'index.html',
-        measurements=[measurement.dict() for measurement in measurements],
-    )
-
-
-@view.post('/upload')
-def upload():
-    if request.files:
-        file = request.files['file']
-        files = {'file': (file.filename, file)}
+    form = FileForm(meta={'csrf': False})
+    if form.validate_on_submit():
+        file = form.pdf_report.data
+        filename = secure_filename(file.filename)
+        files = {'file': (filename, file)}
         timeout = httpx.Timeout(5.0, connect=5.0, read=50.0, write=5.0)
         r = httpx.post("http://185.252.146.250:5000/api/v1/upload/", files=files, timeout=timeout)
         if r.status_code != '201':
@@ -28,5 +29,8 @@ def upload():
             return redirect(url_for('index.index'))
         flash('got done')
         return redirect(url_for('index.index'))
-    flash('No file')
-    return redirect(url_for('index.index'))
+    return render_template(
+        'index.html',
+        measurements=[measurement.dict() for measurement in measurements],
+        form=form,
+    )
